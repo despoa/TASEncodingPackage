dac993518684e94dfb3ea68e07b555a8e1e304a9
Zach <yamkoo@gmail.com>
2021-05-11 07:48:10 -0700

    Avisynth+64, Multithreading, Etc. (#7)
    * Updates for AviSynth+, multithreading, and 64-bitness.
    
    Use AviSynth+ functions for plugin loading. Use new multithreading functions. Update all plugins to 64-bitness. Remove avs2pipemode.exe entirely and replace with ffprobe.exe. Replace all audio encoding with custom ffmpeg.exe.
    
    * Add build files/instructions for custom ffmpeg.
    
    * Remove some comments.
    
    * Fix AAC audio lag. Docs.
    
    * Move docs to their own folders.
    
    * Forgot timecodefps docs.
    
    * x264_x264_feos_edition

--- global.bat
+++ global.bat
@@ -99,10 +99,11 @@ for /f "tokens=2 skip=2 delims== " %%G in ('find "hAspect = " "%~dp0encode.avs"'
 
 ".\programs\replacetext" "encode.avs" "handHeld = true" "handHeld = false"
 ".\programs\replacetext" "encode.avs" "pass = 0" "pass = 1"
-".\programs\avs2pipemod" -info encode.avs > ".\temp\info.txt"
+".\programs\ffprobe" -hide_banner -v error -select_streams v -of default -show_entries stream=width,height,r_frame_rate encode.avs > ".\temp\info.txt"
 
-for /f "tokens=2" %%G in ('FIND "width" "%~dp0temp\info.txt"') do (set width=%%G)
-for /f "tokens=2" %%G in ('FIND "height" "%~dp0temp\info.txt"') do (set height=%%G)
+for /f "tokens=2 delims==" %%G in ('FINDSTR "width" "%~dp0temp\info.txt"') do (set width=%%G)
+for /f "tokens=2 delims==" %%G in ('FINDSTR "height" "%~dp0temp\info.txt"') do (set height=%%G)
+
 set /a "SAR_w=%ar_w% * %height%"
 set /a "SAR_h=%ar_h% * %width%"
 set VAR=%SAR_w%:%SAR_h%
@@ -113,6 +114,7 @@ goto ENCODE_OPTIONS
 : handHeld_SAR
 set VAR=1:1
 ".\programs\replacetext" "encode.avs" "handHeld = false" "handHeld = true"
+".\programs\ffprobe" -hide_banner -v error -select_streams v -of default -show_entries stream=r_frame_rate encode.avs > ".\temp\info.txt"
 goto ENCODE_OPTIONS
 
 : ENCODE_OPTIONS
@@ -205,24 +207,27 @@ echo C'mon! Let's finish this already!
 goto get_extrahq_type
 
 : HD
-title Starting up...
-title
-:: Audio ::
-".\programs\avs2pipemod" -wav encode.avs | ".\programs\venc" -q10 - ".\temp\audio_youtube.ogg"
+title Processing YouTube
 echo.
 echo ----------------------------
 echo  Encoding YouTube HD stream
 echo ----------------------------
 echo.
+:: Audio ::
+echo Encoding audio...
+".\programs\ffmpeg" -y -hide_banner -v error -stats -i encode.avs -vn -c:a libvorbis -q 10 ".\temp\audio_youtube.ogg"
+
 :: Video ::
+echo Encoding video...
 ".\programs\replacetext" "encode.avs" "hd = false" "hd = true"
-".\programs\avs2pipemod" -y4mp encode.avs | ".\programs\x264_x64" --qp 5 -b 0 --keyint infinite --output ".\temp\video_youtube.mkv" --demuxer y4m -
+".\programs\x264_x64" --qp 5 -b 0 --keyint infinite --output ".\temp\video_youtube.mkv" encode.avs
 ".\programs\replacetext" "encode.avs" "hd = true" "hd = false"
+
 :: Muxing ::
 ".\programs\mkvmerge" -o ".\output\encode_youtube.mkv" --compression -1:none ".\temp\video_youtube.mkv" ".\temp\audio_youtube.ogg"
 
 if "%VIRTUALBOY%"=="1" (set VBPREF="_vb_") else (set VBPREF="_")
-if "%VIRTUALBOY%"=="1" (".\programs\ffmpeg" -i ".\output\encode_youtube.mkv" -c copy -metadata:s:v:0 stereo_mode=1 ".\output\encode%VBPREF%youtube.mkv")
+if "%VIRTUALBOY%"=="1" (".\programs\mkvmerge.exe" -o ".\output\encode%VBPREF%youtube.mkv" --compression -1:none --stereo-mode 0:1 ".\temp\video_youtube.mkv" ".\temp\audio_youtube.ogg")
 
 echo.
 echo -----------------------------
@@ -234,94 +239,99 @@ rem start "" cmd /c echo todo ^| "%~dp0programs\tvcman.exe" "%~dp0output\encode%
 if "%EncodeChoice%"=="3" goto Defaults
 
 : 10bit444
-title Starting up...
-title
-:: Audio ::
-".\programs\avs2pipemod" -wav encode.avs | ".\programs\sox" -t wav - -t wav - trim 0.0065 | ".\programs\opusenc" --bitrate 64 --padding 0 - ".\temp\audio.opus"
-echo.
-echo ----------------------
-echo  Generating timecodes
-echo ----------------------
-:: Timecodes ::
-".\programs\replacetext" "encode.avs" "i444 = false" "i444 = true"
-".\programs\replacetext" "encode.avs" "pass = 0" "pass = 1"
-".\programs\avs2pipemod" -benchmark encode.avs
-".\programs\replacetext" "encode.avs" "pass = 1" "pass = 2"
+title Processing Modern
 echo.
 echo --------------------------------
 echo  Encoding 10bit444 downloadable
 echo --------------------------------
 echo.
+:: Audio ::
+echo Encoding audio...
+".\programs\ffmpeg" -y -hide_banner -v error -stats -i encode.avs -vn -c:a libopus -b:a 64K ".\temp\audio.opus"
+
+:: Timecodes ::
+echo Generating timecodes...
+".\programs\replacetext" "encode.avs" "i444 = false" "i444 = true"
+".\programs\replacetext" "encode.avs" "pass = 0" "pass = 1"
+".\programs\ffmpeg" -hide_banner -v error -stats -i encode.avs -an -f null -
+".\programs\replacetext" "encode.avs" "pass = 1" "pass = 2"
+
 :: Video ::
-".\programs\avs2pipemod" -y4mp encode.avs | ".\programs\x264_x64" --threads auto --sar "%VAR%" --crf 20 --keyint 600 --ref 16 --no-fast-pskip --bframes 16 --b-adapt 2 --direct auto --me tesa --merange 64 --subme 11 --trellis 2 --partitions all --no-dct-decimate --input-range pc --range pc --tcfile-in ".\temp\times.txt" -o ".\temp\video.mkv" --colormatrix smpte170m --output-csp i444 --profile high444 --output-depth 10 --demuxer y4m -
+echo Encoding video...
+".\programs\x264_x64" --threads auto --sar "%VAR%" --crf 20 --keyint 600 --ref 16 --no-fast-pskip --bframes 16 --b-adapt 2 --direct auto --me tesa --merange 64 --subme 11 --trellis 2 --partitions all --no-dct-decimate --input-range pc --range pc --tcfile-in ".\temp\times.txt" -o ".\temp\video.mkv" --colormatrix smpte170m --output-csp i444 --profile high444 --output-depth 10 encode.avs
+
 :: Muxing ::
 ".\programs\mkvmerge" -o ".\output\encode.mkv" --timecodes -1:".\temp\times.txt" ".\temp\video.mkv" ".\temp\audio.opus"
  if "%EncodeChoice%"=="1" goto Defaults
 
 : 512kb
-title Starting up...
-title
-:: Audio ::
-".\programs\avs2pipemod" -wav encode.avs | ".\programs\qaac64" -v 0 --he -q 2 --delay -5187s --threading --no-smart-padding - -o ".\temp\audio.mp4"
+title Processing Compatability
 echo.
 echo -------------------------------
 echo  Encoding Archive 512kb stream
 echo -------------------------------
 echo.
+:: Audio ::
+echo Encoding audio...
+".\programs\ffmpeg" -y -hide_banner -v error -stats -i encode.avs -vn -af atrim=start_sample=7107 -c:a libfdk_aac -profile:a aac_he_v2 -vbr 2 ".\temp\audio.mp4"
+
 :: Video ::
+echo Encoding video...
 ".\programs\replacetext" "encode.avs" "pass = 2" "pass = 0"
 ".\programs\replacetext" "encode.avs" "i444 = true" "i444 = false"
-".\programs\avs2pipemod" -y4mp encode.avs | ".\programs\x264_x64" --threads auto --crf 20 --keyint 600 --ref 16 --no-fast-pskip --bframes 16 --b-adapt 2 --direct auto --me tesa --merange 64 --subme 11 --trellis 2 --partitions all --no-dct-decimate --range tv --input-range tv --colormatrix smpte170m -o ".\temp\video_512kb.h264" --demuxer y4m -
+".\programs\x264_x64" --threads auto --crf 20 --keyint 600 --ref 16 --no-fast-pskip --bframes 16 --b-adapt 2 --direct auto --me tesa --merange 64 --subme 11 --trellis 2 --partitions all --no-dct-decimate --range tv --input-range tv --colormatrix smpte170m -o ".\temp\video_512kb.h264" encode.avs
+
 :: Muxing ::
-for /f "tokens=2" %%i in ('^""%~dp0programs\avs2pipemod" -info "%~dp0encode.avs" ^|find "fps"^"') do (set fps=%%i)
+for /f "tokens=2 delims==" %%i in ('FINDSTR "r_frame_rate" "%~dp0temp\info.txt"') do (set fps=%%i)
 for /f %%k in ('^""%~dp0programs\div" %fps%^"') do (set double=%%k)
 ".\programs\mp4box_x64" -hint -add ".\temp\video_512kb.h264":fps=%double% -add ".\temp\audio.mp4" -new ".\output\encode_512kb.mp4"
 goto Defaults
 
 : ExtraHQ_10bit444
-title Starting up...
-title
-:: Extra 10bit444 ::
-:: Audio ::
-".\programs\avs2pipemod" -wav encode.avs | ".\programs\sox" -t wav - -t wav - trim 0.0065 | ".\programs\opusenc" --bitrate 128 --padding 0 - ".\temp\audio_extra.opus"
-echo.
-echo ----------------------
-echo  Generating timecodes
-echo ----------------------
-echo.
-:: Timecodes ::
-".\programs\replacetext" "encode.avs" "pass = 0" "pass = 1"
-".\programs\avs2pipemod" -benchmark encode.avs
-".\programs\replacetext" "encode.avs" "pass = 1" "pass = 2"
+title Processing ExtraHQ Modern
 echo.
 echo ----------------------------------------
 echo  Encoding ExtraHQ 10bit444 downloadable
 echo ----------------------------------------
 echo.
+:: Audio ::
+echo Encoding audio...
+".\programs\ffmpeg" -y -hide_banner -v error -stats -i encode.avs -vn -c:a libopus -b:a 128K ".\temp\audio_extra.opus"
+
+:: Timecodes ::
+echo Generating timecodes...
+".\programs\replacetext" "encode.avs" "pass = 0" "pass = 1"
+".\programs\ffmpeg" -hide_banner -v error -stats -i encode.avs -an -f null -
+".\programs\replacetext" "encode.avs" "pass = 1" "pass = 2"
+
 :: Video ::
+echo Encoding video...
 ".\programs\replacetext" "encode.avs" "i444 = false" "i444 = true"
-".\programs\avs2pipemod" -y4mp encode.avs | ".\programs\x264_x64" --threads auto --sar "%VAR%" --crf 20 --keyint 600 --preset veryslow --input-range pc --range pc --tcfile-in ".\temp\times.txt" -o ".\temp\video_%ExtraScale%x.mkv" --colormatrix smpte170m --output-csp i444 --profile high444 --output-depth 10 --demuxer y4m -
+".\programs\x264_x64" --threads auto --sar "%VAR%" --crf 20 --keyint 600 --preset veryslow --input-range pc --range pc --tcfile-in ".\temp\times.txt" -o ".\temp\video_%ExtraScale%x.mkv" --colormatrix smpte170m --output-csp i444 --profile high444 --output-depth 10 encode.avs
+
 :: Muxing ::
 ".\programs\mkvmerge" -o ".\output\encode_%ExtraScale%x.mkv" --timecodes -1:".\temp\times.txt" ".\temp\video_%ExtraScale%x.mkv" ".\temp\audio_extra.opus"
 if "%ExtraType%" NEQ "3" goto check_more_hqfactors
 
 : ExtraHQ_512kb
-title Starting up...
-title
-:: Extra 512kb ::
-:: Audio ::
-".\programs\avs2pipemod" -wav encode.avs | ".\programs\qaac64" -q 0.5 --delay -2112s --threading --no-smart-padding - -o ".\temp\audio_extra.mp4"
+title Processing ExtraHQ Compatability
 echo.
 echo ---------------------------------------
 echo  Encoding ExtraHQ Archive 512kb stream
 echo ---------------------------------------
 echo.
+:: Audio ::
+echo Encoding audio...
+".\programs\ffmpeg" -y -hide_banner -v error -stats -i encode.avs -vn -af atrim=start_sample=7107 -c:a libfdk_aac -profile:a aac_he_v2 -vbr 5 ".\temp\audio_extra.mp4"
+
 :: Video ::
+echo Encoding video...
 ".\programs\replacetext" "encode.avs" "pass = 2" "pass = 0"
 ".\programs\replacetext" "encode.avs" "i444 = true" "i444 = false"
-".\programs\avs2pipemod" -y4mp encode.avs | ".\programs\x264_x64" --threads auto --crf 20 --keyint 600 --preset veryslow --range tv --input-range tv --colormatrix smpte170m -o ".\temp\video_%ExtraScale%x_512kb.h264" --demuxer y4m -
+".\programs\x264_x64" --threads auto --crf 20 --keyint 600 --preset veryslow --range tv --input-range tv --colormatrix smpte170m -o ".\temp\video_%ExtraScale%x_512kb.h264" encode.avs
+
 :: Muxing ::
-for /f "tokens=2" %%i in ('^""%~dp0programs\avs2pipemod" -info "%~dp0encode.avs" ^|find "fps"^"') do (set fps=%%i)
+for /f "tokens=2 delims==" %%i in ('FINDSTR "r_frame_rate" "%~dp0temp\info.txt"') do (set fps=%%i)
 for /f %%k in ('^""%~dp0programs\div" %fps%^"') do (set double=%%k)
 ".\programs\mp4box_x64" -hint -add ".\temp\video_%ExtraScale%x_512kb.h264":fps=%double% -add ".\temp\audio_extra.mp4" -new ".\output\encode_%ExtraScale%x_512kb.mp4"
 goto check_more_hqfactors
---
+++ programs/docs/ffmpeg/readme.txt
@@ -0,0 +1,7 @@
+The versions of ffmpeg and ffprobe included in this package were custom built
+using the excellent tool at: https://github.com/m-ab-s/media-autobuild_suite
+
+Included in this archive are two sets of configuration files for the above tool
+that will generate the ffmpeg and ffprobe executables. Once the media-autobuild
+tool has been set-up and run once, either set of two files can be dropped into
+the build folder, and the tool will generate the executable.
